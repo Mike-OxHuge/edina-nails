912af139f484e01361f9fdf228dfcfb74db99feb
Mike-OxHuge <mikelitoris34@icloud.com>
2021-09-17 17:13:56 +0200

    init

---
+++ pages/about/index.jsx
@@ -0,0 +1,41 @@
+import Card from "@material-ui/core/Card";
+import Box from "@material-ui/core/Box";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+const about = () => {
+  return (
+    <>
+      <Box>
+        <Card variant="outlined">
+          <CardContent>
+            <Typography
+              sx={{ fontSize: 14 }}
+              color="text.secondary"
+              gutterBottom
+            >
+              Word of the Day
+            </Typography>
+            <Typography variant="h5" component="div">
+              be•nev•o•lent
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              adjective
+            </Typography>
+            <Typography variant="body2">
+              well meaning and kindly.
+              <br />
+              {'"a benevolent smile"'}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      </Box>
+    </>
+  );
+};
+
+export default about;
